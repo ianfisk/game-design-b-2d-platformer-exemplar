60ade50864149a9359523b5371265fcb5a73aba1
Ian Fisk <i.fisk32@gmail.com>
2025-03-29 09:07:01 -0600

    Add character jumping.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1f;
+    public float jumpImpulseForce = 2f;
     public LayerMask groundMask;
     public float groundCastLength = 1f;
 
     private bool isGrounded;
     private float horizontalMovement;
+    private float jumpForce = 0f;
 
     private Rigidbody2D rb;
 
@@ -32,6 +34,14 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb.velocityX = speed * horizontalMovement;
+
+        // We want to add force to rigid bodies in FixedUpdate because Unity does all physics
+        // calculations and updates immediately after FixedUpdate.
+        if (jumpForce > 0)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpForce = 0f;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -42,5 +52,9 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
+        if (context.ReadValueAsButton() && isGrounded)
+        {
+            jumpForce = jumpImpulseForce;
+        }
     }
 }
