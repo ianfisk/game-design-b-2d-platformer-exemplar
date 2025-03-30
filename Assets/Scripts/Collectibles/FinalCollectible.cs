1293a36b7862c8ed8d6ceb69883ede7906aef1d0
Ian Fisk <i.fisk32@gmail.com>
2025-03-30 13:33:32 -0600

    Load EndScene over x seconds, and slow player during that time (example async code).

--- Assets/Scripts/Collectibles/FinalCollectible.cs
+++ Assets/Scripts/Collectibles/FinalCollectible.cs
@@ -1,16 +1,54 @@
+using System;
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinalCollectible : MonoBehaviour, ICollectible
 {
+    private bool isCollected = false;
+
     public int PointsValue => 0;
 
     public void OnCollected(CollectiblesBag bag)
     {
-        gameObject.SetActive(false);
-        Debug.Log($"YOU WIN!!! Total points = {bag.CollectedItems.Sum(x => x.PointsValue)}");
+        if (isCollected)
+        {
+            // The sprite is hidden, but the box collider is still active and may trigger >1 times.
+            return;
+        }
 
-        SceneManager.LoadScene(SceneConstants.EndScene);
+        isCollected = true;
+
+        Debug.Log($"YOU WIN!!! Total points = {bag.CollectedItems.Sum(x => x.PointsValue)}");
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+        StartCoroutine(LoadEndSceneAsync(bag));
+    }
+
+    private IEnumerator LoadEndSceneAsync(CollectiblesBag bag)
+    {
+        const float minSpeed = 3f;
+
+        var asyncLoad = SceneManager.LoadSceneAsync(SceneConstants.EndScene);
+        asyncLoad.allowSceneActivation = false;
+
+        var playerMovement = bag.gameObject.GetComponent<PlayerMovement>();
+
+        // Slow speed from current value down to min over our load scene delay seconds.
+        var endSceneDelaySeconds = 3f;
+        var deltaTime = Time.deltaTime;
+        var frameCountInLoadDelay = endSceneDelaySeconds / Time.deltaTime;
+        var speedDelta = (playerMovement.speed - minSpeed) / frameCountInLoadDelay;
+
+        while (endSceneDelaySeconds > 0)
+        {
+            endSceneDelaySeconds -= deltaTime;
+
+            yield return new WaitForSeconds(deltaTime);
+            playerMovement.speed = Math.Max(playerMovement.speed - speedDelta, minSpeed);
+        }
+
+        asyncLoad.allowSceneActivation = true;
     }
 }
